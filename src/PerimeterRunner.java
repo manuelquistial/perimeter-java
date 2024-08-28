93c04345842a2ad28cb45fcb64e3b8ae15b2ea2f
Manuel Quistial <manuel.quistialj@gmail.com>
2024-08-27 20:06:22 -0500

    add new methods

--- src/PerimeterRunner.java
+++ src/PerimeterRunner.java
@@ -19,11 +19,68 @@ public class PerimeterRunner {
         return totalPerim;
     }
 
+    public int getNumPoints(Shape s){
+        int numPoints = 0;
+        for (Point currPt : s.getPoints()) {
+            numPoints += 1;
+        }
+
+        return numPoints;
+    }
+
+    public double getAverageLength(Shape s){
+        double perimeter = getPerimeter(s);
+        int numPoints = getNumPoints(s);
+
+        return perimeter / numPoints;
+    }
+
+    public double getLargestSide(Shape s){
+        Point prevPt = s.getLastPoint();
+        double prevDist = 0.0;
+        // For each point currPt in the shape,
+        for (Point currPt : s.getPoints()) {
+            // Find distance from prevPt point to currPt
+            double currDist = prevPt.distance(currPt);
+            // Update prevPt to be currPt
+            prevPt = currPt;
+
+            if(currDist > prevDist) {
+                prevDist = currDist;
+            }
+        }
+
+        return prevDist;
+    }
+
+    public double getLargestX(Shape s){
+        double largestX = 0.0;
+        // For each point currPt in the shape,
+        for (Point currPt : s.getPoints()) {
+            // Find distance from prevPt point to currPt
+            double currX = currPt.getX();
+
+            if(currX > largestX) {
+                largestX = currX;
+            }
+        }
+
+        return largestX;
+    }
+
     public void testPerimeter () {
         FileResource fr = new FileResource();
         Shape s = new Shape(fr);
         double length = getPerimeter(s);
+        int numPoints = getNumPoints(s);
+        double averageLength = getAverageLength(s);
+        double largestSide = getLargestSide(s);
+        double largestX = getLargestX(s);
         System.out.println("perimeter = " + length);
+        System.out.println("points = " + numPoints);
+        System.out.println("the average length of a side in your shape is " + averageLength);
+        System.out.println("largest side " + largestSide);
+        System.out.println("Largest X " + largestX);
     }
 
     public static void main (String[] args) {
